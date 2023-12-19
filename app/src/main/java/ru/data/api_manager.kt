ed2943a6b662275b383cc650b8b3ad8d0843db28
danon <kirillovdanil578@gmail.com>
2023-12-20 01:14:40 +0500

    API условно работает, функционал таблиц переписан

---
+++ app/src/main/java/ru/data/api_manager.kt
@@ -0,0 +1,34 @@
+package ru.data
+
+import io.swagger.client.models.GameScore
+import ru.data.apis.DefaultApi
+
+public const val BASE_URL = "https://shiny-space-fiesta-ppw9xv6g6w92rw7q-8080.app.github.dev/"    // Сервер с API
+
+/**
+ * Метод возвращает максимальный результат пользователя username
+ */
+fun getUserRank(username: String): String {
+    var rank = ""
+    try {
+        val api = DefaultApi(BASE_URL)
+        rank = api.userRankGet(username).userRank.toString()
+        rank += " место"
+    } catch (e: Exception) {
+        rank = "--- место"
+    }
+    return rank
+}
+
+/**
+ * Метод записывает результат пользователя
+ */
+fun postUserResult(username: String, diff: Int, score: String) {
+    try {
+        val api = DefaultApi(BASE_URL)
+        var res = GameScore(username, diff, score)
+        api.scoresUploadPost(res)
+    } catch (e: Exception) {
+        return
+    }
+}
--- app/src/main/java/ru/progect/rollingmaze/activities/CMainMenuActivity.kt
+++ app/src/main/java/ru/progect/rollingmaze/activities/CMainMenuActivity.kt
@@ -13,10 +13,10 @@ class CMainMenuActivity : AppCompatActivity() {
     private lateinit var editTextUserId: EditText
     private var username : String = ""
 
+    /**
+     * Инициализируем активность
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
-        /**
-         * Инициализируем активность
-         */
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -28,28 +28,28 @@ class CMainMenuActivity : AppCompatActivity() {
         editTextUserId.setText(username)
     }
 
+    /**
+     * Сохраняем текст из поля ввода для имения пользователя
+     */
     override fun onSaveInstanceState(outState: Bundle) {
-        /**
-         * Сохраняем текст из поля ввода для имения пользователя
-         */
         super.onSaveInstanceState(outState)
         // Сохраняем текст в переменную при уничтожении активности
         outState.putString("username", editTextUserId.text.toString())
     }
 
+    /**
+     * Метод для перехода на активность с игрой
+     */
     fun goToTheGameActivity(view: View){
-        /**
-         * Метод для перехода на активность с игрой
-         */
         val intent = Intent(this, CGameActivity::class.java)
         intent.putExtra("username", editTextUserId.text.toString())
         startActivity(intent)
     }
 
+    /**
+     * Метод для перехода на активность с таблицей рекордов
+     */
     fun goToTheRecordTableActivity(view: View){
-        /**
-         * Метод для перехода на активность с таблицей рекордов
-         */
         val intent = Intent(this, CRecordTableActivity::class.java)
         intent.putExtra("username", editTextUserId.text.toString())
         startActivity(intent)
